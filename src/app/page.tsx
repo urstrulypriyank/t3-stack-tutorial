545891a86b0fdb0681db5e0599bd7a8f3934f1fc
priyank rai <type2email@gmail.com>
2023-10-24 19:35:02 +0530

    removing posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
 
-import { CreatePost } from "~/app/_components/create-post";
+
 import { api } from "~/trpc/server";
 
 export default async function Home() {
-  const hello = await api.post.hello.query({ text: "from tRPC" });
+  const hello = await api.post.hello.query();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
