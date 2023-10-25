6af03f4928be8175763145e5983d0f8f441721ae
priyank rai <type2email@gmail.com>
2023-10-25 09:09:34 +0530

    removing curd showcase boilerplate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,34 +5,16 @@ import { api } from "~/trpc/server";
 
 export default async function Home() {
   const hello = await api.post.hello.query({ text: "from tRPC" });
-
+  console.log(hello, "from hello");
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-     
-        <div className="flex flex-col items-center gap-2">
-          <p className="text-2xl text-white">
-            {hello ? hello.greeting : "Loading tRPC query..."}
-          </p>
-        </div>
+      <div className="flex flex-col items-center gap-2">
+        <p className="text-2xl text-white">
+          {hello ? hello.greeting : "Loading tRPC query..."}
+        </p>
+      </div>
 
-        <CrudShowcase />
-      
+      <CrudShowcase />
     </main>
-  );
-}
-
-async function CrudShowcase() {
-  const latestPost = await api.post.getLatest.query();
-
-  return (
-    <div className="w-full max-w-xs">
-      {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}</p>
-      ) : (
-        <p>You have no posts yet.</p>
-      )}
-
-      <CreatePost />
-    </div>
   );
 }
